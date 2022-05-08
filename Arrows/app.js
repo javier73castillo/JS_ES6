480ee5b65488d0877f0378fae65f5b22b42d0da6
Javi Castillo <87362812+javier73castillo@users.noreply.github.com>
2022-05-08 21:20:01 +0200

    FIRST COMMIT JSES6

---
+++ Arrows/app.js
@@ -0,0 +1,32 @@
+// Crea una arrow function que tenga dos parametros a y b y 
+// que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
+// por consola la suma de los dos parametros.
+
+// 1.1 Ejecuta esta función sin pasar ningún parametro
+// 1.2 Ejecuta esta función pasando un solo parametro
+// 1.3 Ejecuta esta función pasando dos parametros
+
+const obj = {
+  a:5,
+  b:10,
+  sumar: function() {
+    return (this.a + this.b);
+    }
+  }
+
+let sumarParametros = (a1,b1) => {
+  const objeto = obj;
+  if (a1 == undefined || b1 == undefined)
+      {
+        return (objeto.a + objeto.b)
+      }
+  else {
+        objeto.a = a1;
+        objeto.b = b1;
+        return (objeto.a + objeto.b);}
+    }
+      
+  console.log(sumarParametros());
+  
+
+
